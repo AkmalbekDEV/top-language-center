1148e186164460dc83ae21a1d216c258cd41fca4
AkmalbekDEV <akmalyusufov32@gmail.com>
2024-08-23 22:23:27 +0500

    lessons changed

--- src/components/Lessons.jsx
+++ src/components/Lessons.jsx
@@ -11,7 +11,7 @@ const Lessons = () => {
                 <Tabs variant='soft-rounded' colorScheme='blue'>
                     <TabList display={'flex'} alignItems={'center'} justifyContent={'center'}>
                         <Tab fontSize={'25px'}>General</Tab>
-                        <Tab fontSize={'25px'}>Movies</Tab>
+                        <Tab fontSize={'25px'}>Series</Tab>
                     </TabList>
 
                     <TabPanels>
@@ -20,23 +20,23 @@ const Lessons = () => {
                             <div className='grid grid-cols-4 max-sm:grid-cols-2 gap-5 mt-5'>
                                 <Link to={'https://drive.google.com/file/d/1zquYLiVrLJ2JRJRyuYpq5C9ySL7uz7ef/view?usp=drive_link'} className='border-2 p-5 rounded-xl hover:shadow-xl transition-all'>
                                     <img src="https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1724112000&semt=ais_hybrid" alt="" />
-                                    <h1 className='text-center text-xl mt-5'>Book 1</h1>
+                                    <h1 className='text-center text-xl max-sm:text-base mt-5'>Pre-Intermediate English</h1>
                                 </Link>
                                 <Link to={'https://drive.google.com/file/d/1YnZCO9bXSnvn70PIHEeLIMbhzQDrnXLl/view?usp=drive_link'} className='border-2 p-5 rounded-xl hover:shadow-xl transition-all'>
                                     <img src="https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1724112000&semt=ais_hybrid" alt="" />
-                                    <h1 className='text-center text-xl mt-5'>Book 2</h1>
+                                    <h1 className='text-center text-xl max-sm:text-base mt-5'>Intermediate English</h1>
                                 </Link>
                                 <Link to={'https://drive.google.com/file/d/1wEIDnkJKwrvH9StBcI3F4ZvRBJmmhewi/view?usp=drive_link'} className='border-2 p-5 rounded-xl hover:shadow-xl transition-all'>
                                     <img src="https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1724112000&semt=ais_hybrid" alt="" />
-                                    <h1 className='text-center text-xl mt-5'>Book 3</h1>
+                                    <h1 className='text-center text-xl max-sm:text-base mt-5'>Grammar B1</h1>
                                 </Link>
                                 <Link to={'https://drive.google.com/file/d/1twlUYpnTPdBd0HTflaiAqalim9Ob9OYM/view?usp=drive_link'} className='border-2 p-5 rounded-xl hover:shadow-xl transition-all'>
                                     <img src="https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1724112000&semt=ais_hybrid" alt="" />
-                                    <h1 className='text-center text-xl mt-5'>Book 4</h1>
+                                    <h1 className='text-center text-xl max-sm:text-base mt-5'>Grammar B2</h1>
                                 </Link>
                             </div>
                         </TabPanel>
-                        <TabPanel marginTop={'5'}>
+                        <TabPanel>
                             <div className='grid grid-cols-4 max-sm:grid-cols-2 gap-5 mt-5'>
                                 {Movies.map((item, index) => (
                                     <Link key={index} to={item.link} className='border-2 p-5 rounded-xl hover:shadow-xl transition-all'>
