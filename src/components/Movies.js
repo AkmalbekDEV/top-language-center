c8f8aad3a3073bf44275f4d5f774318bf5d4e5f7
AkmalbekDEV <akmalyusufov32@gmail.com>
2024-08-24 13:24:01 +0500

    change

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,73 +1,73 @@
 const Movies = [
     {
         id: 1,
-        title: "Movie 1",
+        title: "Друзья (1994)",
         link: "https://rezka.ag/series/comedy/1733-druzya-1994.html#t:238-s:1-e:1",
         img: "https://img.freepik.com/free-vector/home-movie-background-with-popcorn_1419-1852.jpg"
     },
     {
         id: 2,
-        title: "Movie 2",
+        title: "Как я встретил вашу маму (2005)",
         link: "https://rezka.ag/series/comedy/1822-kak-ya-vstretil-vashu-mamu-2005.html#t:238-s:1-e:1",
         img: "https://img.freepik.com/free-vector/home-movie-background-with-popcorn_1419-1852.jpg"
     },
     {
         id: 3,
-        title: "Movie 3",
+        title: "Эмили в Париже (2020)",
         link: "https://rezka.ag/series/drama/35729-emili-v-parizhe-2020.html#t:238-s:1-e:1",
         img: "https://img.freepik.com/free-vector/home-movie-background-with-popcorn_1419-1852.jpg"
     },
     {
         id: 4,
-        title: "Movie 4",
+        title: "Элита (2018)",
         link: "https://rezka.ag/series/drama/28615-elita-2018.html#t:238-s:1-e:1",
         img: "https://img.freepik.com/free-vector/home-movie-background-with-popcorn_1419-1852.jpg"
     },
     {
         id: 5,
-        title: "Movie 5",
+        title: "Оборотень / Волчонок (2011)",
         link: "https://rezka.ag/series/horror/1742-oboroten-2011.html#t:238-s:1-e:1",
         img: "https://img.freepik.com/free-vector/home-movie-background-with-popcorn_1419-1852.jpg"
     },
     {
         id: 6,
-        title: "Movie 6",
+        title: "Дневники вампира (2009)",
         link: "https://rezka.ag/series/fantasy/74-dnevniki-vampira-2009.html#t:238-s:1-e:1",
         img: "https://img.freepik.com/free-vector/home-movie-background-with-popcorn_1419-1852.jpg"
     },
     {
         id: 7,
-        title: "Movie 7",
+        title: "Теория большого взрыва (2007)",
         link: "https://rezka.ag/series/comedy/1154-teoriya-bolshogo-vzryva-2007.html#t:238-s:1-e:1",
         img: "https://img.freepik.com/free-vector/home-movie-background-with-popcorn_1419-1852.jpg"
     },
     {
         id: 8,
-        title: "Movie 8",
+        title: "Люцифер (2016)",
         link: "https://rezka.ag/series/fantasy/11428-lyucifer-2016.html#t:238-s:1-e:1",
         img: "https://img.freepik.com/free-vector/home-movie-background-with-popcorn_1419-1852.jpg"
     },
     {
         id: 9,
-        title: "Movie 9",
+        title: "Люпен (2021)",
         link: "https://rezka.ag/series/detective/35817-lyupen-2021.html#t:238-s:1-e:1",
         img: "https://img.freepik.com/free-vector/home-movie-background-with-popcorn_1419-1852.jpg"
     },
     {
         id: 10,
-        title: "Movie 10",
+        title: "Форс-мажоры / Костюмы в Законе (2011)",
         link: "https://rezka.ag/series/drama/78-fors-mazhory-2011.html#t:238-s:1-e:1",
         img: "https://img.freepik.com/free-vector/home-movie-background-with-popcorn_1419-1852.jpg"
     },
     {
         id: 11,
-        title: "Movie 11",
+        title: "Игра престолов (2011)",
         link: "https://rezka.ag/series/fantasy/45-igra-prestolov-2011.html#t:238-s:1-e:1",
         img: "https://img.freepik.com/free-vector/home-movie-background-with-popcorn_1419-1852.jpg"
     },
     {
         id: 12,
-        title: "Movie 12",
+        title: "Сплетница (2007)",
         link: "https://rezka.ag/series/drama/2378-spletnica-2007.html#t:238-s:1-e:1",
         img: "https://img.freepik.com/free-vector/home-movie-background-with-popcorn_1419-1852.jpg"
     },
