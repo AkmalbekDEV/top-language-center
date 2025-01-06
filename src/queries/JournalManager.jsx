83fa4ca166155ce8d828188097af1ae19a04c1e3
amiryusupov <amiryusupov.070@gmail.com>
2025-01-06 18:37:11 +0500

    feat: useAddJournal to create multiple journal entries for week 1
    - The useAddJournal function in JournalManager.jsx has been refactored to create multiple journal entries for week 1. Previously, it only created a single entry. Now, if the weekId is 1, it creates journal entries for all 8 weeks.
    
    - If the weekId is 1, the function creates journal entries for weeks 1-8 by iterating through a loop. Each entry is assigned a newId using the getNextJournalId function and is saved to the database using setDoc. The created journal entries are returned as an array.
    
    - If the weekId is not 1, the function creates a single journal entry as before. The entry is assigned a newId, saved to the database, and returned.
    
    - The onSuccess callback has also been updated to invalidate the appropriate queries based on the weekId. If the weekId is 1, all week queries are invalidated. Otherwise, only the current week query is invalidated.
    
    - This change improves the functionality of useAddJournal by allowing it to create multiple journal entries for week 1, providing more flexibility and convenience for users.

--- src/queries/JournalManager.jsx
+++ src/queries/JournalManager.jsx
@@ -107,28 +107,68 @@ export const useJournalManager = () => {
   const useAddJournal = () => {
     return useMutation({
       mutationFn: async ({ journalType, journalData, groupId, weekId }) => {
-        const newId = await getNextJournalId(journalType);
         const collectionName = getCollectionName(journalType);
 
-        const newJournalData = {
-          ...journalData,
-          id: newId,
-          groupRef: `groups/${groupId}`,
-          weekRef: `journal_weeks/journal_weeks-00${weekId}`,
-          createdAt: serverTimestamp(),
-          updatedAt: serverTimestamp(),
-        };
+        // if it's week 1, create entries for all 8 weeks
+        if (weekId === "1") {
+          const createdJournals = [];
 
-        await setDoc(doc(db, collectionName, newId), newJournalData);
-        return newJournalData;
+          // Create journals for weeks 1-8
+          for (let week = 1; week <= 8; week++) {
+            const newId = await getNextJournalId(journalType);
+            const weekRef = `journal_weeks/journal_weeks-00${week}`;
+
+            const journalDataForWeek = {
+              ...journalData,
+              id: newId,
+              groupRef: `groups/${groupId}`,
+              weekRef: weekRef,
+              createdAt: serverTimestamp(),
+              updatedAt: serverTimestamp(),
+            }
+
+            await setDoc(doc(db, collectionName, newId), journalDataForWeek);
+            createdJournals.push(journalDataForWeek)
+          }
+
+          return createdJournals;
+        } else {
+          // For other weeks, just create a single journal entry
+          const newId = await getNextJournalId(journalType);
+
+          const newJournalData = {
+            ...journalData,
+            id: newId,
+            groupRef: `groups/${groupId}`,
+            weekRef: `journal_weeks/journal_weeks-00${weekId}`,
+            createdAt: serverTimestamp(),
+            updatedAt: serverTimestamp(),
+          };
+
+          await setDoc(doc(db, collectionName, newId), newJournalData);
+          return newJournalData;
+        }
       },
       onSuccess: (_, { journalType, groupId, weekId }) => {
-        queryClient.invalidateQueries([
-          "journals",
-          journalType,
-          groupId,
-          weekId,
-        ]);
+        // if it was week 1, invalidate all weeks
+        if (weekId === "1") {
+          for (let week = 1; week <= 8; week++) {
+            queryClient.invalidateQueries([
+              "journals",
+              journalType,
+              groupId,
+              String(week),
+            ]);
+          }
+        } else {
+          // Otherwise just invalidate the current week
+          queryClient.invalidateQueries([
+            "journals",
+            journalType,
+            groupId,
+            weekId,
+          ]);
+        }
       },
     });
   };
@@ -190,147 +230,3 @@ export const useJournalManager = () => {
     useDeleteJournal,
   };
 };
-
-// import { createContext, useCallback, useState } from "react";
-// import Axios from "../api";
-// import {
-//   journalAddUrl,
-//   journalDeleteUrl,
-//   journalEditUrl,
-//   journalRelationUrl,
-//   weeksListUrl,
-// } from "../utils/urls";
-// import PropTypes from "prop-types";
-
-// export const JournalContext = createContext();
-
-// const JournalProvider = ({ children }) => {
-//   const [journal, setJournal] = useState([]);
-//   const [week, setWeek] = useState([]);
-//   const [error, setError] = useState(null);
-//   const [loading, setLoading] = useState(false);
-
-//   const getJournals = useCallback(async (journalType, groupId, weekId) => {
-//     setLoading(true);
-//     setError(null);
-//     try {
-//       const response = await Axios.get(
-//         journalRelationUrl(journalType, groupId, weekId)
-//       );
-//       const data = response.data;
-//       const groupJournals = data.filter(
-//         (journal) =>
-//           journal.group.id.toString() === groupId &&
-//           journal.journal_week_id.toString() === weekId
-//       );
-
-//       if (groupJournals.length > 0) {
-//         setJournal({
-//           journals: groupJournals,
-//           groups: groupJournals[0].group,
-//           groupName: groupJournals[0].group.name,
-//         });
-//       } else {
-//         setJournal({
-//           journals: [],
-//           groups: [],
-//           groupName: "",
-//         });
-//       }
-//     } catch (err) {
-//       setError(err);
-//       setJournal({
-//         journals: [],
-//         groups: [],
-//         groupName: "",
-//       });
-//     } finally {
-//       setLoading(false);
-//     }
-//   }, []);
-
-//   const getWeeks = useCallback(async () => {
-//     setLoading(true);
-//     setError(null);
-//     try {
-//       const response = await Axios.get(weeksListUrl);
-//       setWeek(response.data);
-//     } catch (err) {
-//       setError(err);
-//     } finally {
-//       setLoading(false);
-//     }
-//   }, []);
-
-//   const postJournal = async (journalType, body) => {
-//     setLoading(true);
-//     setError(null);
-//     try {
-//       const response = await Axios.post(journalAddUrl(journalType), body);
-//       setJournal((prevState) => ({
-//         ...prevState,
-//         journals: [...prevState.journals, response.data],
-//       }));
-//     } catch (error) {
-//       setError(error);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-//   const editJournal = async (journalType, body, id) => {
-//     setLoading(true);
-//     setError(null);
-//     try {
-//       const response = await Axios.patch(journalEditUrl(journalType, id), body);
-//       const updatedJournal = response.data;
-//       setJournal((prevState) => ({
-//         ...prevState,
-//         journals: prevState.journals.map((journal) =>
-//           journal.id === id ? updatedJournal : journal
-//         ),
-//       }));
-//     } catch (err) {
-//       setError(err);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-//   const deleteJournal = async (journalType, id) => {
-//     setLoading(true)
-//     setError(null)
-//     try {
-//       await Axios.delete(journalDeleteUrl(journalType, id));
-//       setJournal((prevJournal) => ({
-//         ...prevJournal,
-//         journals: prevJournal.journals.filter((journal) => journal.id !== id),
-//       }));
-//     } catch (err) {
-//       setError(err);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-//   return (
-//     <JournalContext.Provider
-//       value={{
-//         journal,
-//         week,
-//         loading,
-//         error,
-//         getJournals,
-//         getWeeks,
-//         postJournal,
-//         editJournal,
-//         deleteJournal
-//       }}
-//     >
-//       {children}
-//     </JournalContext.Provider>
-//   );
-// };
-
-// JournalProvider.propTypes = {
-//   children: PropTypes.node.isRequired,
-// };
-
-// export default JournalProvider;
